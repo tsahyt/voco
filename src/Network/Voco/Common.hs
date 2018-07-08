f26066f53f670319b675da51ce198ba968a93ba4
Paul Ogris <paul@tsahyt.com>
2018-07-08 11:24:02 +0200

    Add delayed join functionality

--- src/Network/Voco/Common.hs
+++ src/Network/Voco/Common.hs
@@ -1,16 +1,40 @@
 -- | Module defining common or even ubiquitous bots.
 module Network.Voco.Common
     ( pingpong
+    , delayedJoin
+    , delayedJoinBatch
     ) where
 
 import Control.Lens
+import Data.List.NonEmpty (NonEmpty)
+import qualified Data.List.NonEmpty as N
 import Network.Voco.Action
 import Network.Voco.Bot
 import Network.Voco.Combinators
+import Network.Yak
 import Network.Yak.Client
+import Network.Yak.Responses
 
 -- | A bot responding to ping.
 pingpong :: Monad m => Bot m Ping ()
 pingpong = do
     ping <- query
     pong (ping ^. pingServer1) (ping ^. pingServer2)
+
+-- | A bot executing joins to a given list of channels after the welcome
+-- message. All channels are sent in one go! See 'delayedJoinBatch' for a
+-- batched variant.
+delayedJoin :: Monad m => NonEmpty Channel -> Bot m RplWelcome ()
+delayedJoin = join
+
+chunksOf :: Int -> [a] -> [[a]]
+chunksOf _ [] = []
+chunksOf n xs =
+    let (front, back) = splitAt n xs
+    in front : chunksOf n back
+
+-- | Like 'delayedJoin' but sends join messages in batches of @n@.
+delayedJoinBatch :: Monad m => Int -> NonEmpty Channel -> Bot m RplWelcome ()
+delayedJoinBatch n cs =
+    let chunked = chunksOf n . N.toList $ cs
+     in mapM_ (join . N.fromList) chunked
